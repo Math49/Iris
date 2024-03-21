d2361fb2a16bd5c12cd414132b8796bdbac1ca9a
AubinManceau <aubinmanceau0@gmail.com>
2024-03-21 11:25:45 +0100

    article

--- resources/js/Pages/Blog/ArticlePattern.jsx
+++ resources/js/Pages/Blog/ArticlePattern.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Link, Head } from "@inertiajs/react";
-import axios from 'axios';
-import { Inertia } from '@inertiajs/inertia';
 import Header from '@/Layouts/Header';
+import Footer from '@/Layouts/Footer';
+import { CircleChevronLeft } from 'lucide-react';
+import { Link} from "@inertiajs/react";
 
 export default function ArticlePattern({auth, blog, users}) {
 
@@ -11,30 +10,33 @@ export default function ArticlePattern({auth, blog, users}) {
     return (
         <>
         <Header auth={auth} />
-
-        <div className='flex flex-col items-center justify-center'>
-            
-            <div className="container">
-                    <div key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>{blog.content}</p>
-                        <p>{users && users.find(user => user.id === blog.user_id).name}</p>
-                        <p>{new Date(blog.created_at).toLocaleDateString('fr-FR')}</p>
-
-                        {blog.media_type === 'image' ? (
-                            <img src={blog.media} alt={blog.title} />
-                        ) : (
+        <div className='px-52 pt-12'>
+            <div className="text-gray-900 flex mb-5">
+                <Link href={route('blog.index')} className='flex items-center gap-x-1'>
+                    <CircleChevronLeft size={16} className='text-bleu' />
+                    Retour
+                </Link>
+            </div>
+            <div key={blog.id}>
+                <h1 className='font-semibold uppercase text-4xl text-center mb-3'>{blog.title}</h1>
+                <p className='text-sm font-bold text-center'>publié le {new Date(blog.created_at).toLocaleDateString('fr-FR')}</p>
+                <div className='mt-6'>
+                    {blog.media ? (
+                        blog.media_type === 'video' ? (
                             <video controls>
                                 <source src={blog.media} type="video/mp4" />
                                 Your browser does not support the video tag.
                             </video>
-                        )}
-                    </div>
-                        
-            </div>
-            
+                        ) : (
+                            <img src={blog.media} alt={blog.title} />
+                        )
+                    ) :
+                    null}
+                </div>
+                <p className='mt-14'>{blog.content}</p>
+            </div>     
         </div>
-
+        <Footer />
         </>
     )
 }
--- resources/js/Pages/Blog/BlogView.jsx
+++ resources/js/Pages/Blog/BlogView.jsx
@@ -1,42 +1,98 @@
 import React, { useState, useEffect } from 'react';
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Link, Head } from "@inertiajs/react";
-import axios from 'axios';
-import { Inertia } from '@inertiajs/inertia';
+import { Link } from "@inertiajs/react";
 import Header from '@/Layouts/Header';
+import Footer from '@/Layouts/Footer';
+import image from '../../../img/gem0701938-jpg-29468.jpg';
 
 export default function BlogView({auth, blogs, users}) {
-
-    console.log(auth.user.roles);
 
     return (
         <>
         <Header auth={auth} />
 
-        <div className='flex flex-col items-center justify-center'>
-
-            {auth.user && auth.user.roles[0] === 'admin' && (
-                <Link href={route("blog.add")}>Add Blog</Link>
-            )}
-            
-            
-            <div className="container">
-                
-                {blogs && blogs.map(blog => (
-                    <Link href={route("blog.show", blog.id)} key={blog.id}>
-                        <div  className='p-4 bg-white m-4 border-2 border-gray-300 sm:rounded-lg'>
-                            <h2 className='font-bold mb-4 '>{blog.title}</h2>
-                            <p>{users && users.find(user => user.id === blog.user_id).name}</p>
-                            <p>{new Date(blog.created_at).toLocaleDateString('fr-FR')}</p>
-                        </div>
-                    </Link>
-                
-                ))}
-
+        <div className='px-24 mt-20'>
+            <div className='mb-20'>
+                <h1 className='text-noir uppercase text-6xl leading-[70px]'>Les médias pour <br /> s'informer sur le cyberharcèlement</h1>                
+                <div className='mt-6'>
+                    <p>Dans de blog patatattatatattatattatattatattataa</p>
+                </div>
             </div>
-            
-        </div>
 
+            <div>
+                <h2 className='uppercase text-4xl mb-10'>Nos dernières actions</h2>
+                <div className='flex justify-between'>
+                    {blogs && blogs.slice(0, 2).map(blog => (
+                    <Link href={route("blog.show", blog.id)} key={blog.id} className='border-[.5px] border-bleu shadow-[0px_4px_16px_0px_#00000010] p-5 rounded-md w-[28vw] h-[60vh] flex flex-col justify-between'>
+                        {blog.media_type === 'image' ?
+                            <img className='rounded-md w-[100%] h-[35%]' src={blog.media} alt="" />
+                        :
+                            <video className='rounded-md w-[100%] h-[35%]' src={blog.media}></video>
+                        }
+                        <div className='mt-4'>
+                            <h3 className='text-2xl line-clamp-2 leading-6 font-semibold'>{blog.title}</h3>
+                            <p className='mt-2 text-sm line-clamp-4'>{blog.content}</p>
+                        </div>
+                        <p className='text-sm mt-4 font-semibold'>publié le {new Date(blog.created_at).toLocaleDateString('fr-FR')}</p>
+                    </Link>
+                    ))}
+                    <div className='flex flex-col justify-between'>
+                        {blogs && blogs.slice(2, 4).map(blog => (
+                        <Link href={route("blog.show", blog.id)} key={blog.id} className='border-[.5px] border-bleu shadow-[0px_4px_16px_0px_#00000010] p-5 rounded-md w-[28vw] flex gap-x-4 h-[28.5vh]'>
+                                {blog.media_type === 'image' ?
+                                    <img className='rounded-md h-[100%] w-[30%]' src={blog.media} alt="" />
+                                :
+                                    <video className='rounded-md h-[100%] w-[30%]' src={blog.media}></video>
+                                }
+                                <div className='w-full flex flex-col justify-between'>
+                                    <div>
+                                        <h3 className='text-2xl line-clamp-2 leading-6 font-semibold'>{blog.title}</h3>
+                                        <p className='mt-2 text-sm line-clamp-4'>{blog.content}</p>
+                                    </div>
+                                    <p className='text-sm mt-4 font-semibold'>publié le {new Date(blog.created_at).toLocaleDateString('fr-FR')}</p>
+                                </div>
+                        </Link>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-between mt-20'>
+                <div className='w-[64%]'>
+                    <h2 className='uppercase text-4xl mb-10'>Dernier posts</h2>
+                    {blogs && blogs.map(blog => (
+                        <Link href={route("blog.show", blog.id)} key={blog.id} className='border-[.5px] border-bleu shadow-[0px_4px_16px_0px_#00000010] p-5 rounded-md w-[100%] h-[50vh] flex flex-col justify-between mb-6'>
+                            {blog.media_type === 'image' ?
+                                <img className='rounded-md w-[100%] h-[45%]' src={blog.media} alt="" />
+                            :
+                                <video className='rounded-md w-[100%] h-[45%]' src={blog.media}></video>
+                            }
+                            <div className='mt-4'>
+                                <h3 className='text-2xl line-clamp-2 leading-6 font-semibold'>{blog.title}</h3>
+                                <p className='mt-2 text-sm line-clamp-2'>{blog.content}</p>
+                            </div>
+                            <p className='text-sm mt-4 font-semibold'>publié le {new Date(blog.created_at).toLocaleDateString('fr-FR')}</p>
+                        </Link>
+                    ))}
+                </div>
+                <div className='w-[33%]'>
+                    <h2 className='uppercase text-4xl mb-10'>Posts populaire</h2>
+                    {blogs && blogs.map(blog => (
+                        <Link href={route("blog.show", blog.id)} key={blog.id} className='border-[.5px] border-bleu shadow-[0px_4px_16px_0px_#00000010] p-5 rounded-md w-[100%] h-[50vh] flex flex-col justify-between mb-6'>
+                            {blog.media_type === 'image' ?
+                                <img className='rounded-md w-[100%] h-[45%]' src={blog.media} alt="" />
+                            :
+                                <video className='rounded-md w-[100%] h-[45%]' src={blog.media}></video>
+                            }
+                            <div className='mt-4'>
+                                <h3 className='text-2xl line-clamp-2 leading-6 font-semibold'>{blog.title}</h3>
+                                <p className='mt-2 text-sm line-clamp-2'>{blog.content}</p>
+                            </div>
+                            <p className='text-sm mt-4 font-semibold'>publié le {new Date(blog.created_at).toLocaleDateString('fr-FR')}</p>
+                        </Link>
+                    ))}
+                </div>
+            </div>        
+        </div>
+        <Footer />
         </>
     )
 }
